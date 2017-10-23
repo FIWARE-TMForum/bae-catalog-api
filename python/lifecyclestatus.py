063b57bdddf4a9fe8830f37a44d4e79c0c59f6db
Samuel García <samgh96@gmail.com>
2017-10-23 13:22:31 +0200

    add lifecyclestatus

---
+++ python/lifecyclestatus.py
@@ -0,0 +1,10 @@
+class Lifecyclestatus:
+    def __init__(self, i):
+        self.__status = i
+        self.transitions = {'In Study': ["In Design"], 'In Design': ["In Test"],
+                            'In Test': ["In Design", "Rejected", "Active"],
+                            'Active': ["Retired", "Launched"], 'Launched': ["Retired"],
+                            'Retired': ["Obsolete"], 'Rejected': [], 'Obsolete': []}
+        
+    def can_transition_from(self, transition):
+        return [x for x in self.transitions[self.__status] if x == transition] != []
