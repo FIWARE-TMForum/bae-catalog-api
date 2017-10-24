7c8360746f9464aa3635ec4a1bcffa80a37cdfef
Samuel García <samgh96@gmail.com>
2017-10-24 14:00:14 +0200

    add timestamp

---
+++ python/timestamp.py
@@ -0,0 +1,12 @@
+import re
+import datetime
+
+class Timestamp:
+    def __init__(self, d):
+        r = re.search('(\d{4})-(\d{2})-(\d{2})T(\d{1,2}):(\d{2}):(\d{2})([+|-])(\d{2}):\d{2}', d)
+        offset = datetime.timezone(datetime.timedelta(0,0,0,0,0,int("{}{}".format(r.group(7), r.group(8))),0))
+        self.date = datetime.datetime(int(r.group(1)), int(r.group(2)), int(r.group(3)), int(r.group(4)),
+                                       int(r.group(5)), int(r.group(6)), 0, offset)
+
+    def date(self):
+        return self.date.__str__()
